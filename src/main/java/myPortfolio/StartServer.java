696e064ff9febcda7b6a4cf87fa2f759f84f6dfd
Jared Gurr <jared@gurrfamily.com>
2020-10-24 11:54:26 -0700

    minor update

---
+++ src/main/java/myPortfolio/StartServer.java
@@ -0,0 +1,29 @@
+package main.java.myPortfolio;
+
+//TODO: Find out why this isn't working
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+
+public class StartServer {
+
+    public static void main(final String[] args) {
+        final StartServer startServer = new StartServer();
+    }
+
+    private final Vertx vertx;
+    private final boolean debug = true;
+
+    StartServer() {
+
+        final VertxOptions options = new VertxOptions();
+        if(debug) {
+            options.setBlockedThreadCheckInterval(Long.MAX_VALUE >> 2);
+        }
+
+        vertx = Vertx.vertx(options);
+        vertx.deployVerticle(new HttpServerVerticle());
+        vertx.deployVerticle(new MariadbVerticle());
+
+    }
+
+}
