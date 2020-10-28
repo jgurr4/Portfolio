f8aa5024c53e0564a9d0d8e44636dc99e81660b7
Jared Gurr <jared@gurrfamily.com>
2020-10-28 08:57:55 -0700

    Got html/css pages working. images still don't work.

--- src/main/java/myPortfolio/HttpServerVerticle.java
+++ src/main/java/myPortfolio/HttpServerVerticle.java
@@ -1,4 +1,126 @@
-package main.java.myPortfolio;
+package myPortfolio;
 
-public class HttpServerVerticle {
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import io.vertx.reactivex.ext.web.handler.StaticHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.vertx.reactivex.core.Vertx.*;
+import io.reactivex.*;
+import io.reactivex.annotations.Nullable;
+import io.vertx.core.json.JsonObject;
+import io.vertx.reactivex.*;
+import io.vertx.reactivex.core.*;
+import io.vertx.reactivex.core.http.*;
+import io.vertx.reactivex.ext.web.*;
+import io.vertx.reactivex.ext.web.handler.BodyHandler;
+import io.vertx.reactivex.core.eventbus.EventBus;
+
+
+public class HttpServerVerticle extends AbstractVerticle {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HttpServerVerticle.class);
+
+    @Override
+    public Completable rxStart() {
+        LOGGER.info("Http Verticle is starting. Make sure MariaDB is running.");
+        HttpServer server = vertx.createHttpServer();
+
+        Router router = Router.router(vertx);
+        router.route().handler(StaticHandler.create());
+        router.get("/static/*").handler(this::staticHandler);
+        router.route().handler(BodyHandler.create());
+        router.post("/api/contact").handler(this::busHandler);
+        final int port = 8081;
+        final Single<HttpServer> rxListen = server
+            .requestHandler(router)
+            .rxListen(port)
+            .doOnSuccess(e -> {
+                LOGGER.info("HTTP server running on port " + port);
+            })
+            .doOnError(e -> {
+                LOGGER.error("Could not start a HTTP server", e.getMessage());
+            });
+
+        return rxListen.ignoreElement();
+    }
+
+    private void staticHandler(RoutingContext context) {
+
+        final HttpServerResponse response = context.response();
+        final HttpServerRequest request = context.request();
+
+        @Nullable
+        String path = request.path();
+        try {
+            LOGGER.debug("GET " + path);
+            path = path.substring(1);
+            final InputStream stream = Thread.currentThread().getContextClassLoader().getResourceAsStream(path);
+            if (stream != null) {
+                final String text = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))
+                    .lines()
+                    .collect(Collectors.joining("\n"));
+                if (path.endsWith(".html")) {
+                    response.putHeader("Content-Type", "text/html");
+                } else if (path.endsWith(".css")) {
+                    response.putHeader("Content-Type", "text/css");
+                } else if (path.endsWith(".png")) {
+                    response.putHeader("Content-Type", "image/png");
+                } else if (path.endsWith(".jpg")) {
+                    response.putHeader("Content-Type", "image/jpg");
+                } else {
+                    response.end("<html><body>Error filetype unknown: " + path + "</body></html>");
+                }
+                response.end(text);
+                response.setStatusCode(200);
+                LOGGER.debug("text =" + text);
+            } else {
+                LOGGER.warn("Resource not found: " + path);
+                response.setStatusCode(404);
+                response.end();
+            }
+        } catch (Exception e) {
+            LOGGER.error("Problem fetching static file: " + path, e);
+            response.setStatusCode(502);
+            response.end();
+        }
+    }
+
+    private void busHandler(RoutingContext context) {
+
+        try {
+            final EventBus eb = vertx.eventBus();
+
+            final HttpServerRequest request = context.request();
+            final HttpServerResponse response = context.response();
+            final MultiMap params = request.params();
+
+            //This puts the params from http headers into json object.
+            JsonObject object = new JsonObject();
+            for (Map.Entry<String, String> entry : params.entries()) {
+                object.put(entry.getKey(), entry.getValue());
+            }
+
+            eb.rxRequest("mariadb", object.encode())
+                .subscribe(e -> {
+                        LOGGER.debug("HttpServer Verticle Received reply: " + e.body());
+                    },
+                    err -> {
+                        LOGGER.debug("Error communicating to MariadbVerticle. " + err.getMessage());
+                        // TODO: try doing this part with only response.end and see if that successfully gets page to refresh without needing to do redirect.
+                        response.setStatusCode(303);
+                        response.putHeader("Location", "/jared.html");
+                        response.end();
+                    });
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+    }
 }
+
---
+++ src/main/java/myPortfolio/MariadbVerticle.java
@@ -0,0 +1,63 @@
+package myPortfolio;
+
+import io.vertx.core.json.JsonObject;
+import io.vertx.mysqlclient.MySQLConnectOptions;
+import io.vertx.reactivex.mysqlclient.MySQLPool;
+import io.vertx.reactivex.sqlclient.Row;
+import io.vertx.reactivex.sqlclient.RowSet;
+import io.vertx.reactivex.sqlclient.Tuple;
+import io.vertx.sqlclient.PoolOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.reactivex.*;
+import io.vertx.reactivex.core.*;
+import io.vertx.reactivex.core.eventbus.EventBus;
+import io.vertx.reactivex.core.eventbus.Message;
+
+import java.util.List;
+
+public class MariadbVerticle extends AbstractVerticle {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MariadbVerticle.class);
+
+    public Completable rxStart() {
+        final EventBus eb = vertx.eventBus();
+        eb.consumer("mariadb", this::handleInsert);
+        return Completable.complete();
+    }
+
+    private void handleInsert(Message<String> message) {
+        LOGGER.debug("MariadbVerticle received message : " + message.body());
+        final JsonObject json = JsonObject.mapFrom(message.body());
+        final List list = json.mapTo(List.class);
+/*      final String name = json.getString("name");
+        final String business = json.getString("business"); */
+        final MySQLConnectOptions connectOptions = new MySQLConnectOptions()
+                .setPort(3306)
+                .setHost("the-host")
+                .setDatabase("the-db")
+                .setUser("user")
+                .setPassword("secret");
+
+        PoolOptions poolOptions = new PoolOptions();
+
+        MySQLPool client = MySQLPool.pool(connectOptions, poolOptions);
+
+        client
+                .preparedQuery("INSERT INTO contacts(name, business, position, callback, phone, interview_date) values (?, ?, ?, ?, ?, ?)")
+                .execute(Tuple.tuple(list), ar -> {
+                    if (ar.succeeded()) {
+                        RowSet<Row> result = ar.result();
+                        LOGGER.debug("Got " + result.size() + " rows ");
+                        message.reply("success");
+                    } else {
+                        LOGGER.debug("Failure: " + ar.cause().getMessage());
+                        message.reply("failure");
+                    }
+
+                    client.close();
+                });
+    }
+
+}
