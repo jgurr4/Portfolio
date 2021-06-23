bd5318e9bfe235c5ceb3f5ce4c9b2ee947f50f6f
jgurr4 <jared@gurrfamily.com>
2021-06-23 07:27:21 -0700

    Fixed url requiring /static/jared.html to run.

--- src/main/java/myPortfolio/HttpServerVerticle.java
+++ src/main/java/myPortfolio/HttpServerVerticle.java
@@ -24,120 +24,130 @@ import io.vertx.reactivex.core.eventbus.EventBus;
 
 public class HttpServerVerticle extends AbstractVerticle {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(HttpServerVerticle.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(HttpServerVerticle.class);
 
-    @Override
-    public Completable rxStart() {
-        LOGGER.info("Http Verticle is starting. Make sure MariaDB is running.");
-        HttpServer server = vertx.createHttpServer();
+  @Override
+  public Completable rxStart() {
+    LOGGER.info("Http Verticle is starting. Make sure MariaDB is running.");
+    HttpServer server = vertx.createHttpServer();
 
-        Router router = Router.router(vertx);
-        router.route().handler(StaticHandler.create());
-        router.get("/static/*").handler(this::staticHandler);
+    Router router = Router.router(vertx);
+    router.route().handler(StaticHandler.create());
+    //Add a route for / to route to static/jared.html that way people don't have to type in static/jared.html
+    router.get("/").handler(this::redirect);
+    router.get("/static/*").handler(this::staticHandler);
 //        router.get("/lawn/*").handler(this::lawnHandler);
-        router.route().handler(BodyHandler.create());
-        router.post("/api/contact").handler(this::busHandler);
-        final int port = 8080;
-        final Single<HttpServer> rxListen = server
+    router.route().handler(BodyHandler.create());
+    router.post("/api/contact").handler(this::busHandler);
+    final int port = 8080;
+    final Single<HttpServer> rxListen = server
             .requestHandler(router)
             .rxListen(port)
             .doOnSuccess(e -> {
-                LOGGER.info("HTTP server running on port " + port);
+              LOGGER.info("HTTP server running on port " + port);
             })
             .doOnError(e -> {
-                LOGGER.error("Could not start a HTTP server", e.getMessage());
+              LOGGER.error("Could not start a HTTP server", e.getMessage());
             });
 
-        return rxListen.ignoreElement();
+    return rxListen.ignoreElement();
+  }
+
+  private void redirect(RoutingContext context) {
+    final HttpServerResponse response = context.response();
+    final HttpServerRequest request = context.request();
+    response.putHeader("location", "/static/jared.html");
+    response.setStatusCode(302);
+    response.end();
+  }
+
+  private void staticHandler(RoutingContext context) {
+
+    final HttpServerResponse response = context.response();
+    final HttpServerRequest request = context.request();
+
+    @Nullable
+    String path = request.path();
+    try {
+      LOGGER.debug("GET " + path);
+      path = path.substring(1);
+      boolean isText = true;
+      if (path.endsWith(".html")) {
+        response.putHeader("Content-Type", "text/html");
+      } else if (path.endsWith(".css")) {
+        response.putHeader("Content-Type", "text/css");
+      } else if (path.endsWith(".png")) {
+        response.putHeader("Content-Type", "image/png");
+        isText = false;
+      } else if (path.endsWith(".jpg")) {
+        response.putHeader("Content-Type", "image/jpg");
+        isText = false;
+      } else if (path.endsWith(".pdf")) {
+        response.putHeader("Content-Type", "application/pdf");
+        isText = false;
+      } else {
+        response.setStatusCode(502);
+        response.end("<html><body>Error filetype unknown: " + path + "</body></html>");
+        return;
+      }
+
+      final InputStream stream = Thread.currentThread().getContextClassLoader().getResourceAsStream(path);
+      if (stream == null) {
+        LOGGER.warn("Resource not found: " + path);
+        response.setStatusCode(404);
+        response.end();
+      } else if (isText) {
+        final String text = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))
+                .lines()
+                .collect(Collectors.joining("\n"));
+        response.setStatusCode(200);
+        response.end(text);
+        LOGGER.debug("text =" + text);
+      } else {
+        Buffer buffer = Buffer.buffer(stream.readAllBytes());
+        response.setStatusCode(200);
+        response.end(buffer);
+      }
+    } catch (Exception e) {
+      LOGGER.error("Problem fetching static file: " + path, e);
+      response.setStatusCode(502);
+      response.end();
     }
+  }
 
-    private void staticHandler(RoutingContext context) {
+  private void busHandler(RoutingContext context) {
 
-        final HttpServerResponse response = context.response();
-        final HttpServerRequest request = context.request();
+    try {
+      final EventBus eb = vertx.eventBus();
 
-        @Nullable
-        String path = request.path();
-        try {
-            LOGGER.debug("GET " + path);
-            path = path.substring(1);
-            boolean isText = true;
-            if (path.endsWith(".html")) {
-                response.putHeader("Content-Type", "text/html");
-            } else if (path.endsWith(".css")) {
-                response.putHeader("Content-Type", "text/css");
-            } else if (path.endsWith(".png")) {
-                response.putHeader("Content-Type", "image/png");
-                isText = false;
-            } else if (path.endsWith(".jpg")) {
-                response.putHeader("Content-Type", "image/jpg");
-                isText = false;
-            } else if (path.endsWith(".pdf")) {
-                response.putHeader("Content-Type", "application/pdf");
-                isText = false;
-            } else {
-                response.setStatusCode(502);
-                response.end("<html><body>Error filetype unknown: " + path + "</body></html>");
-                return;
-            }
+      final HttpServerRequest request = context.request();
+      final HttpServerResponse response = context.response();
+      final MultiMap params = request.params();
 
-            final InputStream stream = Thread.currentThread().getContextClassLoader().getResourceAsStream(path);
-            if (stream == null) {
-                LOGGER.warn("Resource not found: " + path);
-                response.setStatusCode(404);
-                response.end();
-            } else if (isText) {
-                final String text = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))
-                    .lines()
-                    .collect(Collectors.joining("\n"));
-                response.setStatusCode(200);
-                response.end(text);
-                LOGGER.debug("text =" + text);
-            } else {
-                Buffer buffer = Buffer.buffer(stream.readAllBytes());
-                response.setStatusCode(200);
-                response.end(buffer);
-            }
-        } catch (Exception e) {
-            LOGGER.error("Problem fetching static file: " + path, e);
-            response.setStatusCode(502);
-            response.end();
-        }
-    }
+      //This puts the params from http headers into json object.
+      JsonObject object = new JsonObject();
+      for (Map.Entry<String, String> entry : params.entries()) {
+        object.put(entry.getKey(), entry.getValue());
+      }
 
-    private void busHandler(RoutingContext context) {
-
-        try {
-            final EventBus eb = vertx.eventBus();
-
-            final HttpServerRequest request = context.request();
-            final HttpServerResponse response = context.response();
-            final MultiMap params = request.params();
-
-            //This puts the params from http headers into json object.
-            JsonObject object = new JsonObject();
-            for (Map.Entry<String, String> entry : params.entries()) {
-                object.put(entry.getKey(), entry.getValue());
-            }
-
-            eb.rxRequest("mariadb", object.encode())
-                .subscribe(e -> {
+      eb.rxRequest("mariadb", object.encode())
+              .subscribe(e -> {
                         LOGGER.debug("HttpServer Verticle Received reply: " + e.body());
                         //TODO Ask how to get this part to successfully refresh the page...
-                            response.setStatusCode(303);
-                            response.putHeader("Location", "/static/jared.html");
-                            response.end();
-                    },
-                    err -> {
+                        response.setStatusCode(303);
+                        response.putHeader("Location", "/static/jared.html");
+                        response.end();
+                      },
+                      err -> {
                         LOGGER.debug("Error communicating to MariadbVerticle. " + err.getMessage());
                         // TODO: try doing this part with only response.end and see if that successfully gets page to refresh without needing to do redirect.
                         response.setStatusCode(303);
                         response.putHeader("Location", "/static/jared.html");
                         response.end();
-                    });
-        } catch (Exception e) {
-            LOGGER.error(e.getMessage(), e);
-        }
+                      });
+    } catch (Exception e) {
+      LOGGER.error(e.getMessage(), e);
     }
+  }
 }
 
